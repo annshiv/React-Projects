a11dd174fe20de14a96166f5759cbd2c1bede591
annshiv <annamalaipalani11@gmail.com>
2021-06-25 15:47:04 +0530

    pure react

--- about-me/src/App.js
+++ about-me/src/App.js
@@ -1,13 +1,11 @@
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="dog" breed="Havanese" />
-      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="cat" breed="Mix" />
+      <SearchParams />
     </div>
   );
 };
