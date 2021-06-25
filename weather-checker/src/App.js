e34b631d6c465ab1f04fbc87d4596f2108f3eeb9
annshiv <annamalaipalani11@gmail.com>
2021-06-25 15:45:11 +0530

    Input fields completed

---
+++ weather-checker/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="heading">Check your weather !!!</h1>
+      <input className="input" type="text" placeholder="City name.." />
+      <input className="input2" type="text" placeholder="Country name.." />
+      <button className="get"> Get weather </button>
+    </div>
+  );
+}
+
+export default App;
