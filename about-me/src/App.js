794d3e5cf52d583441dee588ccb621881e8b35ee
annshiv <annamalaipalani11@gmail.com>
2021-06-28 15:39:48 +0530

    jsx

--- about-me/src/App.js
+++ about-me/src/App.js
@@ -1,11 +1,13 @@
 import { render } from "react-dom";
-import SearchParams from "./SearchParams";
+import Pet from "./Pet";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <SearchParams />
+      <Pet name="Luna" animal="dog" breed="Havanese" />
+      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="cat" breed="Mix" />
     </div>
   );
 };
