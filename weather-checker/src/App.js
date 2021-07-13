df9a37b403cbcb4e3a27057af8a55b3c5a3e507b
annshiv <annamalaipalani11@gmail.com>
2021-07-13 10:41:10 +0530

    React SWR

--- weather-checker/src/App.js
+++ weather-checker/src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
 import "./App.css";
 import Weather from "./components/Weather";
+import useSWR, { SWRConfig } from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 function App() {
   return (
-    <div className="App">
+    // <div className="App">
+    //   <Weather />
+    // </div>
+    <SWRConfig
+      value={{
+        focusThrottleInterval: 20000,
+        fetcher,
+      }}
+    >
       <Weather />
-    </div>
+    </SWRConfig>
   );
 }
 
