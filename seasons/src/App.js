76183ad3f202fc54ab92d9cc91ba680c08977cb1
Annamalai Palani <49332020+annshiv@users.noreply.github.com>
2021-07-21 22:51:53 +0530

    Passings state as Props

--- seasons/src/App.js
+++ seasons/src/App.js
@@ -3,35 +3,22 @@ import "./App.css";
 import SeasonDetails from "./SeasonDetail";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  state = { lat: null, errorMessage: "" };
 
-    this.state = { lat: null, errorMessage: "" };
-
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
 
   render() {
-    // return (
-    //   <div>
-    //     Latitude: {this.state.lat}
-    //     <br />
-    //     Error: {this.state.errorMessage}
-    //   </div>
-    // );
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error : {this.state.errorMessage}</div>;
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude : {this.state.lat}</div>;
+      return <SeasonDetails lat={this.state.lat} />
     }
 
     return <div>Loading ...</div>;
--- seasons/src/SeasonDetail.js
+++ seasons/src/SeasonDetail.js
@@ -1,6 +1,16 @@
 import React from "react";
 
-const SeasonDetails = () => {
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "Summer" : "Winder";
+  } else {
+    return lat > 0 ? "Winter" : "Summer";
+  }
+};
+
+const SeasonDetails = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  console.log(season);
   return <div>Season detail</div>;
 };
 
