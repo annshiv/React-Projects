f06292e76d053137a5755265887682b9e618014a
Annamalai Palani <49332020+annshiv@users.noreply.github.com>
2021-07-26 09:00:25 +0530

    Showing Icons

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "semantic-ui-css/semantic.min.css";
 
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
@@ -12,10 +13,13 @@ const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
   const text =
     season === "winter" ? "Burr, it is chilly" : "Lets hit the beach";
+  const icon = season === "winter" ? "snowflake" : "sun";
 
   return (
     <div>
+      <i className={`${icon} icon`} />
       <h1>{text}</h1>
+      <i className={`${icon} icon`} />
     </div>
   );
 };
