8338ee73c30f733d19bc7001e22dd170e8419642
Annamalai Palani <49332020+annshiv@users.noreply.github.com>
2021-09-28 00:15:36 +0530

    initial commit

---
+++ translate/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div> Hello, This is annamalai</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
