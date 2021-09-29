51af02ea0c24d99c732e1057d353f96fb804bfa9
Annamalai Palani <49332020+annshiv@users.noreply.github.com>
2021-09-29 22:58:21 +0530

    Jsx implemented

--- translate/src/index.js
+++ translate/src/index.js
@@ -1,8 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const submit = () => {
+  return "submit";
+};
+
 const App = () => {
-  return <div> Hello, This is annamalai</div>;
+  return (
+    <div>
+      <label for="name">Enter your name :</label>
+      <input type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {submit()}
+      </button>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
