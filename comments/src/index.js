e55456a5be85a8e0da67fdfbefe7af11b042c642
Annamalai Palani <49332020+annshiv@users.noreply.github.com>
2021-10-04 23:47:20 +0530

    without props

--- comments/src/index.js
+++ comments/src/index.js
@@ -2,7 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  return <div>Hello bro</div>;
+  return (
+    <div className="ui comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Annamalai
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00PM</span>
+          </div>
+          <div className="text">Nice one !</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
