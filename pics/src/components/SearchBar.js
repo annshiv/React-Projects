d45aa1a096191af20ec439003958b5d4f2572fc5
Annamalai Palani <annamalaipalani11@gmail.com>
2022-03-13 01:44:10 +0530

    feat(UI): SearchBar component

---
+++ pics/src/components/SearchBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+          <label>Image search</label>
+            <input type="text" />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
